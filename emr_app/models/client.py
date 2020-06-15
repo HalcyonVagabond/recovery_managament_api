8b7d40732a6b6f468ddb0b6daf9dd87184304957
Jeremy Mattingly <jamattingly@live.com>
2020-06-15 10:40:39 -0500

    mvp models made

---
+++ emr_app/models/client.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.db.models import F
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .provider_type import ProviderType
+
+
+class Client(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=55)
+    address = models.CharField(max_length=255)
+    birth_date = models.DateTimeField(auto_now=False)
+    height = models.IntegerField()
+
+    class Meta:
+        ordering = (F('id').asc(nulls_last=True),)
--- emr_app/models/practitioner.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-from django.db.models import F
-from django.contrib.auth.models import User
-
-class Practitioner(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
